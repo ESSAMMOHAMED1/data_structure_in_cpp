a6048f6c4239dc4895c1c4f47a3b2ba6fec902b0
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-11-15 23:06:16 -0800

    update

---
+++ linked_list.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node
+{
+    int data;
+    node*next;
+};
+node*H = NULL;
+node*C = NULL;
+node*T = NULL;
+/*
++-------+    +-------+    +-------+    +-------+
+| data  | -> | data  | -> | data  | -> | data  |
++-------+    +-------+    +-------+    +-------+
+| next  |    | next  |    | next  |    | NULL  |
++-------+    +-------+    +-------+    +-------+
+
+*/
+
+
+
+// add new node 
+
+void addNewNode(int data){
+
+node * NewNode = new node;
+NewNode -> data = data;
+NewNode->next = nullptr;
+}
+
+
+
+
+int main()
+{
+    
+}
