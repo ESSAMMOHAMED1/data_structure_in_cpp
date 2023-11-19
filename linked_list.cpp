18fd23bd27802c0f359c2b29f37fcce0c5fd3b26
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-11-18 23:30:15 -0800

    update

--- linked_list.cpp
+++ linked_list.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-struct node
+//
+//   A linked list is a linear data structure,
+//   in which the elements are not stored at contiguous memory locations.
+//   The elements in a linked list are linked using pointers
+//
+//- types of linked list
+//  1- Single-linked list
+//    - Each node has data and a reference to the next node.
+//    - It's unidirectional.
+
+// set structure of linked list
+
+struct Node
 {
-    int data;
-    node *next;
+    string name; // set data
+    int number;  // set data
+    Node *next;  // set pointer to linke nodes
 };
-node *H = NULL;
-node *C = NULL;
-node *T = NULL;
 
-/*
-+-------+    +-------+    +-------+    +-------+
-| data  | -> | data  | -> | data  | -> | data  |
-+-------+    +-------+    +-------+    +-------+
-| next  |    | next  |    | next  |    | NULL  |
-+-------+    +-------+    +-------+    +-------+
+Node *H = nullptr; // set main pointer  Head
+Node *T = nullptr; // set main pointer  Tail
+Node *C = nullptr; // set main pointer  Current
 
-*/
+// +-------+    +-------+    +-------+    +-------+
+// | data  | -> | data  | -> | data  | -> | data  |
+// +-------+    +-------+    +-------+    +-------+
+// | next  |    | next  |    | next  |    | NULL  |
+// +-------+    +-------+    +-------+    +-------+
 
-// add new node
+// 2 - Double linked list
 
-void addNewNode(int data)
-{
-
-    node *NewNode = new node;
-    NewNode->data = data;
-    NewNode->next = nullptr;
-}
+// 3 - Circular linked list
 
 int main()
 {
