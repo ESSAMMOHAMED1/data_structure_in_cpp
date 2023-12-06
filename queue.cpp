3b44d4809f537bbe2c6215e2be5c6bd4fd1633d9
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-12-05 20:47:37 -0800

    update

--- queue.cpp
+++ queue.cpp
@@ -1,4 +1,4 @@
-//Queue
+// Queue
 #include <bits/stdc++.h>
 using namespace std;
 struct Node
@@ -12,29 +12,28 @@ Node *F = NULL; // set main pointer  front
 Node *R = NULL; // set main pointer  rear
 Node *C = NULL; // set main pointer  current
 
-
-
-bool Deleted(){
-    if(F==NULL&&R==NULL)
-    return true;
+bool Deleted()
+{
+    if (F == NULL && R == NULL)
+        return true;
     return false;
 }
 
 void EnQueue(string A, int B)
 {
-    C = new Node;      // create new node
-    C->name = A;       // set data of node
-    C->number = B;     // set data of node
-    C->next = NULL;    // set pointer next = null >> case it's a tail (:
-    
+    C = new Node;   // create new node
+    C->name = A;    // set data of node
+    C->number = B;  // set data of node
+    C->next = NULL; // set pointer next = null >> case it's a tail (:
+
     // check if Queue is empty
     if (Deleted())
     {
-        F = R = C; // set front = rear = current 
+        F = R = C; // set front = rear = current
     }
     else
     {
-        R-> next = C; // pointer rear of next -> current
+        R->next = C; // pointer rear of next -> current
         R = C;       // set rear = current
     }
 }
@@ -42,20 +41,19 @@ void EnQueue(string A, int B)
 void Erase_Head()
 {
     C = F;
-    if(F==R){
-    delete C;
-  F =  R = NULL;
-    }  else{
-       // set front = front -> next
-    F = F->next; // set front =>> front of next
-    delete C;    // delete current
-    } 
- 
+    if (F == R)
+    {
+        delete C;
+        F = R = NULL;
+    }
+    else
+    {
+        // set front = front -> next
+        F = F->next; // set front =>> front of next
+        delete C;    // delete current
+    }
 }
 
-
-
-
-int main(){
-    
+int main()
+{
 }
