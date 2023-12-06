21eb13d8ce65cf6a2eae7d2733f8cb2efbc7d852
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2023-12-05 20:16:57 -0800

    update

---
+++ queue.cpp
@@ -0,0 +1,11 @@
+//Queue
+struct Node
+{
+    string name; // set data
+    int number;  // set data
+    Node *next;  // set pointer to linke nodes
+};
+
+Node *H = NULL; // set main pointer  Head
+Node *T = NULL; // set main pointer  Tail
+Node *C = NULL; // set main pointer  Current
